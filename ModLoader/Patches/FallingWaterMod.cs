b297d7c664577b073cb9052d2f7aee23a20d53f2
javisar <javisar@gmail.com>
2018-03-05 17:16:50 +0100

    Some tests

---
+++ ModLoader/Patches/FallingWaterMod.cs
@@ -0,0 +1,46 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace ModLoader
+{           	
+    
+    [HarmonyPatch(typeof(FallingWater))]
+    [HarmonyPatch("gravityScale", PropertyMethod.Getter)]
+    internal class FallingWaterMod
+    {
+        private static readonly FieldInfo gravityScale = AccessTools.Field(typeof(FallingWater), "gravityScale");
+
+        [HarmonyPostfix]
+        public static void NormalGravity(FallingWater __instance, ref float __result)
+        {            
+            Debug.Log(" === FallingWaterMod INI === ");
+
+            __result = 1f;
+
+            Debug.Log(" === FallingWaterMod END === ");
+        }
+     
+    }
+
+    /*
+    [HarmonyPatch(typeof(FallingWater), "onSpawn", new Type[0])]
+    internal class FallingWaterMod
+    {
+
+        private static void Postfix(FallingWater __instance)
+        {
+            Debug.Log(" === FallingWaterMod INI === "+ __instance.GetType().ToString());
+
+            PropertyInfo gravityScale = AccessTools.Property(typeof(FallingWater), "gravityScale");
+            gravityScale.SetValue(__instance, 1.0f,null);
+
+            Debug.Log(" === FallingWaterMod END === ");
+        }
+    }
+    */
+    
+}
---
+++ ModLoader/Patches/NoSteamMod.cs
@@ -0,0 +1,47 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace NoSteamMod
+{
+
+    [HarmonyPatch(typeof(Global), "Awake", new Type[0])]
+    internal class NoSteamMod
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions)
+        {
+            Debug.Log(" === NoSteamMod INI === ");
+
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                CodeInstruction instruction = codes[i];
+                if (instruction.opcode == OpCodes.Call && instruction.operand == typeof(DistributionPlatform).GetMethod("Initialize", new Type[0]))
+                {
+                    break;
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+            Debug.Log(" === NoSteamMod END === ");
+        }
+    }
+
+    /*
+    [HarmonyPatch(typeof(DistributionPlatform), "Initialize", new Type[0])]
+    internal class NoSteamMod
+    {
+
+        private static bool Prefix(DistributionPlatform __instance)
+        {
+            Debug.Log(" === NoSteamMod === ");
+            return false;
+        }
+    }
+    */
+}
