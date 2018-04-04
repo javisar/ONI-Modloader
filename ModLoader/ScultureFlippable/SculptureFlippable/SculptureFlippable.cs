9d6df95f10e18d0b027e50667d1bbd5b8932909b
Midnight-Steam <38061493+Midnight-Steam@users.noreply.github.com>
2018-04-04 00:10:14 -0500

    Added "Sculpture Flippable" mod
    Sculptures can now stare at Duplicants from both sides. Maybe make other things flippable.

---
+++ ModLoader/ScultureFlippable/SculptureFlippable/SculptureFlippable.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+
+namespace SculptureFlippable
+{
+    [HarmonyPatch(typeof(SculptureConfig), "CreateBuildingDef")]
+    public static class SculptureFlippable
+    {
+        public static void Postfix(SculptureConfig __instance, ref BuildingDef __result)
+        {
+            __result.PermittedRotations = PermittedRotations.FlipH;
+        }
+    }
+}
