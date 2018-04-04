89bb165592ff7db1ca90d1aaf79e9baa6d497c34
Midnight-Steam <gniayang41@gmail.com>
2018-04-04 14:38:06 -0500

    Redid my folders

---
+++ ModLoader/DefaultDeconstructBuildings/DefaultDeconstructBuildings.cs
@@ -0,0 +1,32 @@
+﻿using Harmony;
+using System.Collections.Generic;
+
+namespace DefaultDeconstruct
+{
+    [HarmonyPatch(typeof(DeconstructTool), "GetDefaultFilters")] //Method inherited from FilteredDragTool
+    internal static class DefaultDeconstruct
+    {
+        private static void Postfix(DeconstructTool __instance, ref Dictionary<string, ToolParameterMenu.ToggleState> filters)
+        {
+            //Debug.Log("=== DefaultDeconstruct INI ===");
+            //filters.Add(ToolParameterMenu.FILTERLAYERS.ALL, ToolParameterMenu.ToggleState.On);
+            //filters.Add(ToolParameterMenu.FILTERLAYERS.BUILDINGS, ToolParameterMenu.ToggleState.Off);
+
+            //DeconstructTool (DeconstructTool) - TODO Need to get method execution only when DeconstructTool calls it
+            if (__instance.ToString() == "DeconstructTool (DeconstructTool)")
+            {
+                if (filters.ContainsKey(ToolParameterMenu.FILTERLAYERS.BUILDINGS) && filters.ContainsKey(ToolParameterMenu.FILTERLAYERS.ALL))
+                {
+                    filters[ToolParameterMenu.FILTERLAYERS.ALL] = ToolParameterMenu.ToggleState.Off;
+                    filters[ToolParameterMenu.FILTERLAYERS.BUILDINGS] = ToolParameterMenu.ToggleState.On;
+                    Debug.Log("=== DEFAULT DECONSTRUCT Tool set to BUILDINGS ===");
+                }
+                else
+                {
+                    Debug.Log("=== DEFAULT DECONSTRUCT - Could not find Keys");
+                };
+            };
+            //Debug.Log("=== DefaultDeconstruct CHANGED ===");
+        }
+    }
+}
---
+++ ModLoader/WaterPurifierOutputTemp/WaterPurifierOutputTemp.cs
@@ -0,0 +1,34 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace WaterPurifier
+{
+    [HarmonyPatch(typeof(WaterPurifierConfig), "ConfigureBuildingTemplate")]
+    public static class WaterPurifier
+    {
+        public static void Postfix(WaterPurifierConfig __instance, ref GameObject go)
+        {
+            Debug.Log("=============== Water Purifier Changed =======================");
+
+            ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
+            //ElementConverter.OutputElement[] oldOutputElements = elementConverter.outputElements;
+            ElementConverter.OutputElement[] newOutputElements;
+
+            newOutputElements = new ElementConverter.OutputElement[]
+            {
+                new ElementConverter.OutputElement(5f, SimHashes.Water, 0f, true, 0f, 0.5f, true, 0.75f, 255, 0),
+                new ElementConverter.OutputElement(0.2f, SimHashes.ToxicSand, 0f, true, 0f, 0.5f, true, 0.25f, 255, 0)
+            };
+
+            elementConverter.outputElements = newOutputElements;
+
+            //elementConverter = go.AddOrGet<ElementConverter>();
+            //checkNewElements = elementConverter.outputElements;
+            //Debug.Log("Total elements: " + checkNewElements.Count().ToString());
+            //foreach (var item in checkNewElements)
+            //Debug.Log("outputElements: " + item.element.id.ToString());
+
+            Debug.Log("=================== END ======================");
+        }
+    }
+}
