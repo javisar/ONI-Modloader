548ddec80cb6fbcfa8434f0ed1988a28b234497d
javisar <javisar@gmail.com>
2018-10-06 11:01:28 +0200

    v0.5.0

--- Source/ModLoader/ModLoader.cs
+++ Source/ModLoader/ModLoader.cs
@@ -11,7 +11,7 @@ namespace ModLoader
 
     public static class ModLoader
     {
-		public const string ModLoaderVersion = "v0.4.9";
+		public const string ModLoaderVersion = "v0.5.0";
 
 		//public const string AssemblyDir = "Assemblies";
 
